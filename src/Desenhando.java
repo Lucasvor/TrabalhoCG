fc7745d3f858d45370ac4118c41a16b96abb2b73
Anny <Anny@DESKTOP-HENRGNJ>
2019-03-26 11:03:57 -0300

    dda retangulo

--- src/Desenhando.java
+++ src/Desenhando.java
@@ -13,7 +13,6 @@ import java.io.BufferedReader;
 import javax.swing.*;
 
 // Esta é uma ferramenta gráfica de desenho básica no qual se podem criar figuras simples.
-
 public class Desenhando{
 
     public static void main(String args[]){
@@ -28,8 +27,6 @@ public class Desenhando{
     }
 }
 
-// Autor: João Matheus Santos Assis
-
 class JanelaComandos extends JFrame{
 
     // Barra de Ferramenta com os botões necessários
@@ -41,13 +38,10 @@ class JanelaComandos extends JFrame{
     private JButton LimpaTela = new JButton("Limpa Tela");
     private JPanel panel = new JPanel(new BorderLayout());
 
-
-
     private Gerador_Desenho Panel_Desenho = new Gerador_Desenho();
 
     private Color Cor_da_Figura = Color.RED;
     private Color Ultima_Cor = Color.RED;
-
 
     private JLabel Status = new JLabel("  Forma ativa: Retângulo");
     private JLabel Posicao = new JLabel("Posição:      ");
@@ -56,14 +50,10 @@ class JanelaComandos extends JFrame{
     private JRadioButton DDA = new JRadioButton("DDA");
     private ButtonGroup bg = new ButtonGroup();
 
-
     public JanelaComandos() {
         super("Desenvolvedor Gráfico");
-
-
         bg.add(Bresenham);
         bg.add(DDA);
-
 
         panel.setBorder(BorderFactory.createEmptyBorder(0,10,0,10));
         panel.add(Status,BorderLayout.WEST);
@@ -78,7 +68,6 @@ class JanelaComandos extends JFrame{
         Barra_De_Ferramenta.add(LimpaTela);
         Barra_De_Ferramenta.add(Bresenham);
         Barra_De_Ferramenta.add(DDA);
-
 
         // Cor de fundo da Barra de Status
         Status.setBackground(Color.WHITE);
@@ -96,11 +85,7 @@ class JanelaComandos extends JFrame{
         LimpaTela.addActionListener(Eventos);
         Bresenham.addActionListener(Eventos);
         DDA.addActionListener(Eventos);
-
-
     }
-
-
 
     private class Eventos_Desenhando implements ActionListener{
 
@@ -109,9 +94,7 @@ class JanelaComandos extends JFrame{
          um Círculo, 2 será uma reta*/
         int Forma = 0;
 
-
         public void actionPerformed(ActionEvent event) {
-
 
             if (event.getSource()== Retangulo){
                 Forma=0;
@@ -156,16 +139,11 @@ class JanelaComandos extends JFrame{
     }
 }
 
-
-
 class Gerador_Desenho extends JPanel implements MouseListener, MouseMotionListener{
 
     Dimension Dimensao = Toolkit.getDefaultToolkit().getScreenSize();
 
     // Criando local onde ficará armazenadas as imagens.
-
-
-
     private BufferedImage Buffered_da_Imagem = new BufferedImage((int)Dimensao.getWidth(),
             (int)Dimensao.getHeight(), BufferedImage.TYPE_INT_RGB);
 
@@ -187,7 +165,6 @@ class Gerador_Desenho extends JPanel implements MouseListener, MouseMotionListen
     }
 
     private boolean tipo_Algoritmo = true;
-
 
     public Gerador_Desenho(){
 
@@ -230,25 +207,21 @@ class Gerador_Desenho extends JPanel implements MouseListener, MouseMotionListen
         g.dispose();
     }
 
-
-
     public void setForma(int newValor, Color newCor){
         valor = newValor;
         Ultima_Cor = newCor;
     }
-
-
 
     public void paint_retangulo(int x2, int y2){
 
         Graphics2D g_retangulo = Buffered_da_Reta.createGraphics();
         g_retangulo.drawImage(Buffered_da_Imagem, 0, 0, null);
         g_retangulo.setColor(Ultima_Cor);
-
         g_retangulo.setStroke(new BasicStroke(2.0f));
-       
+        
+        if(isTipo_Algoritmo()) {
+        //Bresenham
         int d = 0;
-
         int dx = Math.abs(x2 - x);
         int dy = Math.abs(y2 - y);
         int dx2 = 2 * dx; // slope scaling factors to
@@ -295,15 +268,34 @@ class Gerador_Desenho extends JPanel implements MouseListener, MouseMotionListen
                         d -= dy2;
                     }
                 }
-
-                //Atualizando a tela
             g_retangulo.dispose();
         }
-
+    }else{
+            //DDA
+            int dx = x2 - x;
+            int dy = y2 - y;
+            int steps = Math.abs(dx) > Math.abs(dy) ? Math.abs(dx) : Math.abs(dy);
+            float Xinc = dx/ (float)steps;
+            float Yinc = dy / (float)steps;
+            float Xa = x;
+            float Ya = y;
+            
+            for(int i = 0; i<= steps;i++){
+                if (x2>Xa && y2>Ya)
+                    g_retangulo.drawRect((int)Xa,(int)Ya,x2-(int)Xa,y2-(int)Ya);
+                if (x2>Xa && Ya>y2)
+                    g_retangulo.drawRect((int)Xa,y2,x2-(int)Xa,(int)Ya-y2);
+                if (Xa>x2 && Ya>y2)
+                    g_retangulo.drawRect(x2,y2,(int)Xa-x2,(int)Ya-y2);
+                if (Xa>x2 && y2>Ya)
+                    g_retangulo.drawRect(x2,(int)Ya,(int)Xa-x2,y2-(int)Ya);
+                Xa += Xinc;
+                Ya += Yinc;
+                
+                g_retangulo.dispose();             
+            }
+        }
     }
-
-
-
 
     public void paint_Circulo(int x2, int y2){
 
@@ -324,8 +316,6 @@ class Gerador_Desenho extends JPanel implements MouseListener, MouseMotionListen
 
         g_Circulo.dispose();
     }
-
-
 
     public void paint_Reta(int x2, int y2){
 
@@ -396,11 +386,8 @@ class Gerador_Desenho extends JPanel implements MouseListener, MouseMotionListen
                 Xa += Xinc;
                 Ya += Yinc;
             }
-
         }
-}
-
-
+    }
 
     // Capturando os Eventos com o mouse
     public void mousePressed(MouseEvent e) {
@@ -411,49 +398,34 @@ class Gerador_Desenho extends JPanel implements MouseListener, MouseMotionListen
 
         // Chamando o método Forma
         Forma(e.getX(), e.getY());
-        repaint();
-
+        repaint();      
     }
 
-
     public void mouseReleased(MouseEvent e) {
-
         Forma(e.getX(), e.getY());
 
         Graphics g_Imagem = Buffered_da_Imagem.createGraphics();
         g_Imagem.drawImage(Buffered_da_Reta, 0, 0, null);
         g_Imagem.dispose();
-
-
         repaint();
     }
 
-
     public void mouseClicked(MouseEvent e) {
         x = e.getX();
-        
-        if(e.getY() == y){ // faz com que o desenho só apareça depois que arrastar ao ponto Y
         y = e.getY();
         Forma(e.getX(), e.getY());
         repaint(); // Atualiza a imagem do Jpanel
-        }
     }
 
+    public void mouseEntered(MouseEvent e) {}
 
-    public void mouseEntered(MouseEvent e) {
-    }
-
-
-    public void mouseExited(MouseEvent e) {
-    }
-
+    public void mouseExited(MouseEvent e) {}
 
     public void mouseDragged(MouseEvent e) {
         Forma(e.getX(), e.getY());
         label.setText("Posição: X: "+e.getX()+" Y:"+e.getY());
         repaint();
     }
-
 
     public void mouseMoved(MouseEvent e) {
         label.setText("Posição: X: "+e.getX()+" Y:"+e.getY());
@@ -464,13 +436,11 @@ class Gerador_Desenho extends JPanel implements MouseListener, MouseMotionListen
     }
 
     public void Forma(int x, int y){
-
         if (valor==0)
             paint_retangulo(x, y);
         if (valor==1)
             paint_Circulo(x, y);
         if (valor==2)
             paint_Reta(x, y);
-
     }
 }
